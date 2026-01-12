1c37f34f138588c8032e144cc93f44a732c2f160
jlcrogue <jlcrogue@gmail.com>
2026-01-12 11:40:52 -0700

    Deleted old project folder, updated pom and dependencies.

--- Project-01/src/main/java/org/example/project01/controllers/EmployeeController.java
+++ Project-01/src/main/java/org/example/project01/controllers/EmployeeController.java
@@ -1,7 +1,12 @@
 package org.example.project01.controllers;
 
+import org.example.project01.entities.Employee;
 import org.example.project01.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +15,10 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    @PostMapping("/employees")
+    public ResponseEntity<Employee> insert(@RequestBody Employee employee) {
+        employee = this.employeeService.insert(employee);
+        if(employee != null) return new ResponseEntity<>(employee, HttpStatus.CREATED);
+        else return new ResponseEntity<>(new Employee(), HttpStatus.BAD_REQUEST);
+    }
 }
--- Project-01/src/main/java/org/example/project01/services/EmployeeService.java
+++ Project-01/src/main/java/org/example/project01/services/EmployeeService.java
@@ -1,5 +1,6 @@
 package org.example.project01.services;
 
+import org.example.project01.entities.Employee;
 import org.example.project01.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,7 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    public Employee insert(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 }
