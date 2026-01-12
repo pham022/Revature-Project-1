7689db8969e7611a0fc9ed675d7573f4d37efff2
Kim Pham <124918196+pham022@users.noreply.github.com>
2026-01-12 10:11:03 -0600

    Added new Project setup with Maven (#1)
    Co-authored-by: jlcrogue <jlcrogue@gmail.com>

---
+++ Project-01/src/main/java/org/example/project01/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package org.example.project01.repositories;
+
+import org.example.project01.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
