800c6200c8bb7483266a50acb6bddaadef43f690
Kim Pham <124918196+pham022@users.noreply.github.com>
2026-01-16 14:03:20 -0600

    Add getTicketById and getAllTicket (#6)
    * create separate DTO for ticket creation request
    
    * add getTicketById and getAllTicket

--- Project-01/src/main/java/org/example/project01/controllers/TicketController.java
+++ Project-01/src/main/java/org/example/project01/controllers/TicketController.java
@@ -1,24 +1,47 @@
 package org.example.project01.controllers;
 
+import org.example.project01.dto.CreateTicketRequest;
 import org.example.project01.dto.TicketDTO;
 import org.example.project01.entities.Ticket;
 import org.example.project01.services.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/tickets")
 public class TicketController {
 
     @Autowired
     private TicketService ticketService;
 
-    @PostMapping("/ticket")
-    public ResponseEntity<TicketDTO> create(@RequestBody TicketDTO ticketDTO) {
-        Ticket ticket = ticketService.create(ticketDTO);
+    @GetMapping("/{id}")
+    public ResponseEntity<TicketDTO> getTicketById(@PathVariable Long id) {
+        Ticket ticket = ticketService.getById(id);
+
+        if (ticket != null) {
+            TicketDTO responseDTO = new TicketDTO(ticket);
+            return ResponseEntity.ok(responseDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TicketDTO>> getAllTickets() {
+        List<Ticket> tickets = ticketService.getAll();
+        List<TicketDTO> responseDTOs = tickets.stream()
+                .map(TicketDTO::new)
+                .toList();
+        return ResponseEntity.ok(responseDTOs);
+    }
+
+    @PostMapping
+    public ResponseEntity<TicketDTO> create(@RequestBody CreateTicketRequest request) {
+        Ticket ticket = ticketService.create(request);
         TicketDTO responseDTO = new TicketDTO(ticket);
         if (ticket != null) {
             return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
