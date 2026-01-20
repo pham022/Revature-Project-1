b933c7360fd948eb5e74cc50f1c1ec53487b2a9f
Kim Pham <124918196+pham022@users.noreply.github.com>
2026-01-20 13:18:04 -0600

    handle invalid id for getTicketById (#9)
    * added TicketRepository
    
    * working on creating Ticket
    
    * add TicketDTO
    
    * Add ticket creation endpoint with nested EmployeeInfo dto to map only necessary data
    
    * Remove .idea/vcs.xml from Git tracking
    
    * add application.properties back to origin/main
    
    * create separate DTO for ticket creation request
    
    * add getTicketById and getAllTicket
    
    * add exception handling and remove .idea folder from origin/main
    
    * add invalid ticket id exception
    
    * handle invalid id for getTicketById

--- Project-01/src/main/java/org/example/project01/controllers/TicketController.java
+++ Project-01/src/main/java/org/example/project01/controllers/TicketController.java
@@ -4,7 +4,7 @@ import org.example.project01.dto.CreateTicketRequest;
 import org.example.project01.dto.TicketDTO;
 import org.example.project01.entities.Ticket;
 import org.example.project01.services.TicketService;
-import org.example.project01.exceptions.InvalidTicketIdException;
+import org.example.project01.exceptions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +20,7 @@ public class TicketController {
     private TicketService ticketService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<TicketDTO> getTicketById(@PathVariable Long id) {
+    public ResponseEntity<TicketDTO> getTicketById(@PathVariable Long id) throws InvalidTicketIdException {
         Ticket ticket = ticketService.getById(id);
 
         if (ticket != null) {
