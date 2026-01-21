d4af8a141527fb8e338c953018639ba839afe1c0
Kim Pham <124918196+pham022@users.noreply.github.com>
2026-01-21 11:17:00 -0600

    add navbar styling (#12)

--- project-01-frontend/src/App.tsx
+++ project-01-frontend/src/App.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/NavBar';
 import Login from './components/auth/Login';
+import Tickets from './components/tickets/Ticket';
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path="/login" element={<Login/>}/>
-    </Routes>
-    
+      <Navbar/>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/employees" element={<Tickets/>} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- project-01-frontend/src/components/NavBar.tsx
+++ project-01-frontend/src/components/NavBar.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styles from './Navbar.module.css';
+import { useNavigate } from "react-router-dom";
+import styles from "./Navbar.module.css"
 
-export default function Navbar() {
+interface NavbarProps {
+    username?: string;
+}
+
+export default function Navbar({ username }: NavbarProps) {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        // Add logout logic here
+        navigate('/login')
+    }
 
     return (
-        <nav className = {styles.navBar}>
-        <Link className = {styles.navItem} to = "/">Home</Link>
-        <Link className = {styles.navItem} to = "/login">Login</Link>
-        <Link className = {styles.navItem} to = "/register">Register</Link>
-        <Link className = {styles.navItem} to = "/employees">Employees</Link>
-        <Link className = {styles.navItem} to = "/tickets">Tickets</Link>
-        </nav>
+        <header className={styles.header}>
+            <h1>Expense Reimbursement Management System</h1>
+            <div className={styles.headerRight}>
+                <span>Welcome, {username}</span>
+                <button className={styles.logoutBtn} onClick={handleLogout}>
+                    Logout
+                </button>
+            </div>
+        </header>
     )
-
 }
